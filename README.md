2137baa0eb9751d5ef6afc669d7aaa6a1cc65a21
Erald David <pantherald@gmail.com>
2020-10-29 12:08:16 +0700

    Tutorial 5 - Testing

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # first-django-tutorial
 First Django Tutorial App
+
+List of Features:
+- Vote on choice and saved
--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
 # Register your models here.
 # admin.site.register(Question) itu buat register Question model
 admin.site.register(Question)
+admin.site.register(Choice)
